66c0c8dd31dd99da3d0c86067c598fb7ded7347f
Bartprumper <bartprumper@gmail.com>
2026-04-29 16:07:47 +0200

    text is shown as it is being generated instead of in one go. Known issue: response is the exact same each time

--- Doodle drifters 2/Assets/Scripts/LLM/ScenarioTemp.cs
+++ Doodle drifters 2/Assets/Scripts/LLM/ScenarioTemp.cs
@@ -1,15 +1,19 @@
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using LLMUnity;
+using TMPro;
+using Unity.AppUI.UI;
 using UnityEngine;
 
 public class ScenarioTemp : MonoBehaviour
 {
     [SerializeField] private LLMAgent agent;
+    [SerializeField] private TextMeshProUGUI text;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         test();
+        //text.text = "Testing LLM Agent...";
     }
 
     // Update is called once per frame
@@ -20,7 +24,13 @@ public class ScenarioTemp : MonoBehaviour
 
     async private void test()
     {
-        string reply = await agent.Chat("Give me a description of a room in a dungeon, in 2-3 sentences.");
-        Debug.Log("Agent response: " + reply);
+        string reply = await agent.Chat("Give me a description of a room in a dungeon, in 2-3 sentences.", ShowTextOverTime);
+        //Debug.Log("Agent response: " + reply);
+        //text.text = reply;
+    }
+
+    private void ShowTextOverTime(string reply)
+    {
+        text.text = reply;
     }
 }
